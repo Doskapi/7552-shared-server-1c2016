2933f6953abf3df00ab0d50ea3c21eaf91b6829a
sebaber <vicario.sebastian@gmail.com>
2016-04-05 00:59:30 -0300

    test

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
+// app.use( bodyParser.json() );       // to support JSON-encoded bodies
+// app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+//   extended: true
+// }));
 var users = require('./requires/users');
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/users', users);
-// Set a reference to the massive instance on Express' app:
+
 app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function () {
